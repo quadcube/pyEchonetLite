f45113885c1430ff03cfa34169eeaaeab9eb9bb9
James Ooi <quad_cube@hotmail.com>
2019-06-19 18:50:07 +0900

    Removed setPacket(), replaced with a much better createPacket(). Improved dictTraverse.

--- echonet_lite.py
+++ echonet_lite.py
@@ -23,13 +23,13 @@ fd = None
     Echonet Lite Packet Structure
     |   EHD1   |   EHD2   |   TID   |   EDATA (SEOJ,DEOJ,ESV,OPC,EPC1,PDC1,EDT1,...)   |
 """
-class EchonetLite:
+class EchonetLite():
     def __init__(self):
         global ehd, eoj_cgc, eoj_cc, esv, epc, epc_edt, il, fd
         MAX_ECHONET_PACKET_LEN = 64
+        self.current_echonet_packet_len = 0
         self.echonet_packet = [0] * MAX_ECHONET_PACKET_LEN # pre-allocate echonet lite packet structure
-        if ehd == None:
-            self.initEchonetLite()
+        if ehd == None: self.initEchonetLite()
     
     # Initialize Echonet Lite Library
     # Note: 1. Require JSON Echonet Lite property files
@@ -38,57 +38,61 @@ class EchonetLite:
         try:
             with open('json/echonet_lite_EHD.json') as json_file:
                 ehd = json.load(json_file)
-                ehd = self.dictTraverse(ehd, path=None, convert="Hex")
+                ehd = self.dictTraverse(ehd, path=None, convert=True)
             with open('json/echonet_lite_EOJ_CGC.json') as json_file:
                 eoj_cgc = json.load(json_file)
-                eoj_cgc = self.dictTraverse(eoj_cgc, convert="Hex")
+                eoj_cgc = self.dictTraverse(eoj_cgc, convert=True)
             with open('json/echonet_lite_EOJ_CC.json') as json_file:
                 eoj_cc = json.load(json_file)
-                eoj_cc = self.dictTraverse(eoj_cc, convert="Hex")
+                eoj_cc = self.dictTraverse(eoj_cc, convert=True)
             with open('json/echonet_lite_ESV.json') as json_file:
                 esv = json.load(json_file)
-                esv = self.dictTraverse(esv, convert="Hex")
+                esv = self.dictTraverse(esv, convert=True)
             with open('json/echonet_lite_EPC.json') as json_file:
                 epc = json.load(json_file)
-                epc = self.dictTraverse(epc, convert="Hex")
+                epc = self.dictTraverse(epc, convert=True)
             with open('json/echonet_lite_EPC_EDT.json') as json_file:
                 epc_edt = json.load(json_file)
-                epc_edt = self.dictTraverse(epc_edt, convert="Hex")
+                epc_edt = self.dictTraverse(epc_edt, convert=True)
             with open('json/echonet_lite_IL.json') as json_file:
                 il = json.load(json_file)
-                il = self.dictTraverse(il, convert="Hex")
+                il = self.dictTraverse(il, convert=True)
             with open('json/echonet_lite_FD.json') as json_file:
                 fd = json.load(json_file)
-                fd = self.dictTraverse(fd, convert="Hex")
+                fd = self.dictTraverse(fd, convert=True)
         except Exception as e:
             logging.exception("initEchonetLite() exception occurred.")
 
     # Helper: Python Dictionary Traversal
     # Note: 1.
-    #       2.
-    def dictTraverse(self, obj, path=None, callback=None, convert=None, search=None):
+    #       2. key search useful for both epc and eoj_cc json
+    def dictTraverse(self, obj, path=None, callback=None, convert=False, search=None, key=False):
         search_value = None
         # Dictionary Traversal (recursive function)
-        def _inner_traversal(obj, path=None, callback=None, convert=None, search=None, target=None):
+        def _inner_traversal(obj, path=None, callback=None, convert=False, search=None, target=None, key=False):
             nonlocal search_value
-            if path is None:
-                path = []
+            if path is None: path = []
             if isinstance(obj, dict):
                 value = {} #value = {k: self.dictTraverse(v, path + [k], callback, convert, search) for k, v in obj.items()}
                 for k, v in obj.items():
                     #print(f"dict() path {path}, key {k}, value {v}\n\n")
                     if search is not None: # search for value
-                        if all(elem in path for elem in search) and k == target:
+                        if all(elem in path for elem in search) and k == target and key == False:
                             search_value = v
                             raise StopIteration # break all iteration
-                    value.update({k: _inner_traversal(v, path + [k], callback, convert, search, target)})
-                    #print(f"dict() path {path} value {value}\n\n")
+                        elif key == True:
+                            if k == search[0]:
+                                search_value = path[0]
+                                raise StopIteration # break all iteration
+                            elif v == search[0]:
+                                search_value = k
+                                raise StopIteration # break all iteration
+                    value.update({k: _inner_traversal(v, path + [k], callback, convert, search, target, key)})
             elif isinstance(obj, list):
                 value = [] #value = [self.dictTraverse(elem, path + [[]], callback, convert, search) for elem in obj]
                 for elem in obj:
                     #print(f"list() path {path}, elem {elem}\n\n")
-                    value.append(_inner_traversal(elem, path + [[]], callback, convert, search, target))
-                    #print(f"list() path {path} value {value}\n\n")
+                    value.append(_inner_traversal(elem, path + [[]], callback, convert, search, target, key))
             else:
                 #print(f"scalar path {path} value {obj}\n\n")
                 if search is not None: # search for value
@@ -96,7 +100,7 @@ class EchonetLite:
                         search_value = obj
                         raise StopIteration # break all iteration
                 value = obj
-            if convert == "Hex": # convert dict hex strings to int
+            if convert == True: # convert dict hex, bin strings to int
                 try:
                     value = int(value,0)
                 except:
@@ -108,13 +112,16 @@ class EchonetLite:
                 
         if search is not None:
             try:
-                if not isinstance(search, list): # make sure that search variable is a list
-                    search = [search]
-                target = [elem for elem in search if not any(elem1 in elem for elem1 in ['CGC_', 'CC_', 'EPC_'])] # filter off some Echonet Lite constant headers
-                target = 'value' if not target else target[0] # provide default target and ensure it's a single target
-                search_only = [elem for elem in search if any(elem1 in elem for elem1 in ['CGC_', 'CC_', 'EPC_'])] # filter off some Echonet Lite constant
+                if not isinstance(search, list): search = [search] # make sure that search variable is a list
+                try:
+                    target = [elem for elem in search if not any(elem1 in elem for elem1 in ['CGC_', 'CC_', 'EPC_'])] # filter off some Echonet Lite constant headers
+                    target = 'value' if not target else target[0] # provide default target and ensure it's a single target
+                    search_only = [elem for elem in search if any(elem1 in elem for elem1 in ['CGC_', 'CC_', 'EPC_'])] # filter off some Echonet Lite constant
+                except TypeError:
+                    target = None
+                    search_only = search # non-iterable objects are directly passed into search_only (e.g.: int)
                 if not search_only: search_only = search # ensure search is not empty
-                _inner_traversal(obj, path, callback, convert, search_only, target)
+                _inner_traversal(obj, path, callback, convert, search_only, target, key)
             except StopIteration:
                 return search_value
         else:
@@ -138,6 +145,7 @@ class EchonetLite:
                     k += self.echonet_packet[12+i+j+k]
                     j += 1
                 self.echonet_packet[12+k+(i*2)] = epc_code
+            self.current_echonet_packet_len += 1
         else:
             logging.error("setnEPC() invalid n.")
 
@@ -170,6 +178,7 @@ class EchonetLite:
                     k += self.echonet_packet[12+i+j+k]
                     j += 1
                 self.echonet_packet[13+k+(i*2)] = value
+            self.current_echonet_packet_len += 1
         else:
             logging.error("setnPDC() invalid n.")
 
@@ -190,9 +199,10 @@ class EchonetLite:
             return -1
 
     # Set Echonet Lite Property Value
-    # Note: 1. property_data_list must be a list
+    # Note: 1. property_data_list can be any data type or list
     def setnEDT(self, n, property_data_list):
         j, k = 0, 0
+        if not isinstance(property_data_list, list): property_data_list = [property_data_list]  # make sure property_data_list is a list
         if n <= self.echonet_packet[11] and n > 0 and len(property_data_list) == self.getnPDC(n): # check n with OPC & input list is equal to nPDC
             if n == 1:
                 for i in range(self.getnPDC(n)):
@@ -203,6 +213,7 @@ class EchonetLite:
                     j += 1
                 for j in range(self.getnPDC(n)):
                     self.echonet_packet[14+j+k+(i*2)] = property_data_list[j]
+            self.current_echonet_packet_len += len(property_data_list)
         else:
             logging.error("setnEDT() invalid n and/or length of input list is not equal to nPDC.")
 
@@ -226,23 +237,37 @@ class EchonetLite:
         else:
             logging.error("setnEDT() invalid n")
             return -1
-
-    # Set Echonet Lite Packet (without Echonet Lite Property Setting and Data: EPC,PDC,EDT)
-    # Note: 1. tid16 can accept both single 16 bit integer and two 8 bit integer list
-    def setPacket(self, ehd1, ehd2, tid16, seoj_cgc, seoj_cc, seoj_ic, deoj_cgc, deoj_cc, deoj_ic, esv, opc):
-        if type(tid16) is not list:
-            tid16 = [tid16 >> 8, tid16 & 0x00ff]
-        self.echonet_packet[:] = [0 for _ in self.echonet_packet[:]] # reset global var
-        self.echonet_packet[:12] = [ehd1, ehd2, tid16[0], tid16[1], seoj_cgc, seoj_cc, seoj_ic, deoj_cgc, deoj_cc, deoj_ic, esv, opc]
-
-    def getOperationalStatus(self, ip_addr, deoj_cc, deoj_ic=0x01):
+        
+    # Create Echonet Lite Packet
+    # Note: 1. TID can accept both single 16 bit integer and two 8 bit integer list
+    def createPacket(self, DEOJ_CC, TID=0x00, SEOJ_CC='CC_NODE_PROFILE', SEOJ_IC=0x01, DEOJ_IC=0x01, EPC="EPC_OPERATIONAL_STATUS", ESV='ESV_Get', EPC_EDT=()):
         global ehd, eoj_cgc, eoj_cc, esv, epc, epc_edt, il, fd
-        #self.setPacket(ehd['EHD1_ECHONET'], ehd['EHD2_FORMAT1'], 0xFF, eoj_cgc['CGC_PROFILE_CLASS'], eoj_cc['CGC_PROFILE_CLASS']['CC_NODE_PROFILE'], 0x01, eoj_cgc[], eoj_cc[]['CC_TEMPERATURE_SENSOR'], deoj_ic, esv['ESV_Get'], 1)
-        #self.setnEPC(1, )
+        if not isinstance(TID, list): TID = [TID >> 8, TID & 0x00ff] # convert TID to list
+        if not isinstance(EPC, list): EPC = [EPC]  # make sure EPC is a list
+        if not isinstance(EPC_EDT, list): EPC_EDT = [EPC_EDT]  # make sure EPC_EDT is a list
+        OPC = len(EPC) if EPC_EDT == [()] else len(EPC_EDT) # calculate OPC
+        self.echonet_packet[:] = [0 for _ in self.echonet_packet[:]] # reset global var
+        scgc_key = self.dictTraverse(eoj_cc, search=SEOJ_CC, key=True)
+        dcgc_key = self.dictTraverse(eoj_cc, search=DEOJ_CC, key=True)
+        self.echonet_packet[:12] = [ehd['EHD1_ECHONET'], ehd['EHD2_FORMAT1'], TID[0], TID[1], eoj_cgc[scgc_key], eoj_cc[scgc_key][SEOJ_CC], SEOJ_IC, eoj_cgc[dcgc_key], eoj_cc[dcgc_key][DEOJ_CC], DEOJ_IC, esv[ESV], OPC]
+        self.current_echonet_packet_len = 12 # reset current Echonet packet length
+        for i in range(OPC):
+            if EPC_EDT == [()]:
+                target_epc = EPC[i]
+                target_pdc = 0
+            else:
+                target_epc = EPC_EDT[i][0]
+                target_pdc = len(EPC_EDT[i][1])
+            self.setnEPC(i+1, self.dictTraverse(epc_edt, search=target_epc))
+            self.setnPDC(i+1, target_pdc)
+            if not EPC_EDT == [()]:
+                self.setnEDT(i+1, EPC_EDT[i][1])
+        return self.echonet_packet[:self.current_echonet_packet_len]
 
 """
     Echonet Lite Function Test Units
     Note: 1. covers all functions that deals directly with echonet_packet (global variable)
+          2.
 """
 class testEchonetLite(unittest.TestCase):
     def setUp(self): # populate global var with sample header, OPC = 3
@@ -318,34 +343,21 @@ class testEchonetLite(unittest.TestCase):
         self.obj.echonet_packet[len(self.sample):len(self.sample)+5] = [0x01, 0x03, 0xFF, 0xFF, 0xFF] # n = 2, EPC = 0x01, PDC = 0x03, EDT = 0xFF * 3
         self.sample.extend([0x01, 0x03, 0xFF, 0xFF, 0xFF]) # n = 2, EPC = 0x01, PDC = 0x03, EDT = 0xFF * 3
         self.assertEqual(self.obj.getnEDT(2), self.sample[len(self.sample)-3:len(self.sample)])
-    
-    def test_setPacket(self):
-        self.obj.setPacket(0x10, 0x81, [0x00, 0x01], 0x0E, 0xF0, 0x01, 0x00, 0x11, 0x01, 0x62, 3) # test list tid16, tid16 = [0x00, 0x01]
-        self.assertEqual(self.obj.echonet_packet[:len(self.sample)], self.sample)
-        self.obj.setPacket(0x10, 0x81, 0x01, 0x0E, 0xF0, 0x01, 0x00, 0x11, 0x01, 0x62, 3) # test int tid16, tid16 = 0x01
-        self.assertEqual(self.obj.echonet_packet[:len(self.sample)], self.sample)
-        self.obj.setPacket(0x10, 0x81, 0x01FF, 0x0E, 0xF0, 0x01, 0x00, 0x11, 0x01, 0x62, 3) # test int tid16, tid16 = 0x01FF
-        self.sample[2:4] = [0x01, 0xFF] # tid16 = 0x01FF
-        self.assertEqual(self.obj.echonet_packet[:len(self.sample)], self.sample)
-        self.obj.setPacket(0x10, 0x81, 0xF103, 0x0E, 0xF0, 0x01, 0x00, 0x11, 0x01, 0x62, 3) # test int tid16, tid16 = 0xF103
-        self.sample[2:4] = [0xF1, 0x03] # tid16 = 0xF103
-        self.assertEqual(self.obj.echonet_packet[:len(self.sample)], self.sample)
-
-    def test_initEchonetLite_setPacket(self):
-        self.sample = [0x10, 0x81, 0x00, 0xFF, 0x0E, 0xF0, 0x01, 0x00, 0x11, 0x01, 0x62, 3]
-        self.obj.setPacket(ehd['EHD1_ECHONET'], ehd['EHD2_FORMAT1'], 0xFF, eoj_cgc['CGC_PROFILE_CLASS'], eoj_cc['CGC_PROFILE_CLASS']['CC_NODE_PROFILE'], 0x01, eoj_cgc['CGC_SENSOR_RELATED'], eoj_cc['CGC_SENSOR_RELATED']['CC_TEMPERATURE_SENSOR'], 0x01, esv['ESV_Get'], 3)
-        self.assertEqual(self.obj.echonet_packet[:len(self.sample)], self.sample)
 
     def test_dictTraverse(self):
         self.assertEqual(self.obj.dictTraverse(epc_edt, search=["CC_TEMPERATURE_SENSOR"]), 0xE0)
         self.assertEqual(self.obj.dictTraverse(epc_edt, search=["CC_ELECTRIC_ENERGY_SENSOR", "EPC_SMALL_CAPACITY_SENSOR_INSTATANEOUS_ELECTRIC_ENERGY"]), 0xE2)
         self.assertEqual(self.obj.dictTraverse(eoj_cc, search="CC_ELECTRIC_ENERGY_SENSOR"), 0x22) # test non-'value' key dict
         self.assertEqual(self.obj.dictTraverse(ehd, search="EHD2_FORMAT1"), 0x81) # test single level dict without Echonet Lite constant header
+        self.assertEqual(self.obj.dictTraverse(epc_edt, search="EPC_OPERATIONAL_STATUS"), 0x80)
+        self.assertEqual(self.obj.dictTraverse(eoj_cc, search=0x0B, key=True), "CC_AIR_POLLUTION_SENSOR") # search for key
+        self.assertEqual(self.obj.dictTraverse(eoj_cc, search="CC_AIR_POLLUTION_SENSOR", key=True), "CGC_SENSOR_RELATED")
+        self.assertEqual(self.obj.dictTraverse(eoj_cgc, search=0x05, key=True), "CGC_MANAGEMENT_RELATED")
 
-
-
-
-
+    def test_createPacket(self):
+        self.assertEqual(self.obj.createPacket("CC_TEMPERATURE_SENSOR"), [0x10, 0x81, 0x00, 0x00, 0x0E, 0xF0, 0x01, 0x00, 0x11, 0x01, 0x62, 0x01, 0x80, 0x00])
+        self.assertEqual(self.obj.createPacket("CC_TEMPERATURE_SENSOR", EPC=["EPC_OPERATIONAL_STATUS", "EPC_TEMPERATURE_VALUE"]), [0x10, 0x81, 0x00, 0x00, 0x0E, 0xF0, 0x01, 0x00, 0x11, 0x01, 0x62, 0x02, 0x80, 0, 0xE0, 0])
+        #print(self.obj.createPacket("CC_HOME_AIR_CONDITIONER", ESV='ESV_SetC', EPC_EDT=[("EPC_OPERATIONAL_STATUS",0x30), ("EPC_OPERATION_MODE_SETTING", 0x41)]))
 
 
 
@@ -378,6 +390,7 @@ ECHONET_MSG_POWERMETER_D7 = [0x10, 0x81, 0x00, 0x0E, 0x0E, 0xF0, 0x01, 0x02, 0x8
 ECHONET_MSG_POWERMETER_D8 = [0x10, 0x81, 0x00, 0x0F, 0x0E, 0xF0, 0x01, 0x02, 0x87, 0x01, 0x62, 1, 0xD8, 0]
 ECHONET_MSG_POWERMETER_D9 = [0x10, 0x81, 0x00, 0x10, 0x0E, 0xF0, 0x01, 0x02, 0x87, 0x01, 0x62, 1, 0xD9, 0]
 ECHONET_MSG_AIRCOND_GET_SENSETEMP = [0x10, 0x81, 0x00, 0x11, 0x0E, 0xF0, 0x01, 0x01, 0x30, 0x01, 0x62, 1, 0xBB, 0]
+
 
 if __name__ == '__main__':
     unittest.main()
